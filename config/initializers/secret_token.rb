e29e111a581a8b83c84b6d86dcc23922a61c8c32
webmaster <webmaster@homeschoolingit.com>
2011-10-17 08:23:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0122874cda96d0fa1bf028b9c3fa51af05d9956fe34b7db642179ad7f19de222f6e74361ad010de963270b0f4bffe485e62d94e4bc28a93c2fe6e4d91659c62d'
